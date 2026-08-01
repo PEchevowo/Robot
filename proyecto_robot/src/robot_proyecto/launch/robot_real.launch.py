25fc4fd7d7d9114c7d14e0a3834be1a76f462255
PEchevowo <paola.echev.s@gmail.com>
2026-08-01 17:33:39 -0400

    LPM

--- proyecto_robot/src/robot_proyecto/launch/robot_real.launch.py
+++ proyecto_robot/src/robot_proyecto/launch/robot_real.launch.py
@@ -3,14 +3,14 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
-        # 1. Nodo del LiDAR Hokuyo
+        # 1. Nodo del LiDAR Hokuyo (Con el nombre de frame corregido)
         Node(
             package='urg_node',
             executable='urg_node_driver',
             name='urg_node',
             parameters=[{
                 'serial_port': '/dev/ttyACM0',
-                'frame_id': 'laser_frame',
+                'frame_id': 'laser', 
                 'angle_min': -2.35,
                 'angle_max': 2.35
             }]
@@ -21,17 +21,18 @@ def generate_launch_description():
             executable='serial_bridge',
             name='serial_bridge'
         ),
-        # 3. Nodo de Odometría (Cálculo de movimiento)
-        Node(
-            package='robot_proyecto',
-            executable='odom_tf_publisher',
-            name='odom_tf_publisher'
-        ),
-        # 4. Transformada física (Une el láser al chasis del robot)
+        # 3. Transformada física (Une el láser al chasis del robot)
         Node(
             package='tf2_ros',
             executable='static_transform_publisher',
             name='base_to_laser',
-            arguments=['0', '0', '0.1', '0', '0', '0', 'base_link', 'laser_frame']
+            arguments=['0', '0', '0.1', '0', '0', '0', 'base_link', 'laser']
+        ),
+        # 4. Odometría Fija (Puente matemático para Nav2 y SLAM)
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='odom_to_base',
+            arguments=['0', '0', '0', '0', '0', '0', 'odom', 'base_link']
         )
     ])
