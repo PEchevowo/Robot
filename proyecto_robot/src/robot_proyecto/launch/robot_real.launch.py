e2b96edef4c25b9eb0884bb7a786db8d2b2de0df
PEchevowo <paola.echev.s@gmail.com>
2026-08-01 16:48:36 -0400

    Create robot_real.launch.py

---
+++ proyecto_robot/src/robot_proyecto/launch/robot_real.launch.py
@@ -0,0 +1,24 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        # Nodo del LiDAR Hokuyo
+        Node(
+            package='urg_node',
+            executable='urg_node_driver',
+            name='urg_node',
+            parameters=[{
+                'serial_port': '/dev/ttyACM1', # Cambiar a ACM0 o ACM1 según corresponda
+                'frame_id': 'laser_frame',
+                'angle_min': -2.35, # Los Hokuyo suelen tener mayor apertura
+                'angle_max': 2.35
+            }]
+        ),
+        # Nodo del puente serial para mover los motores vía Arduino
+        Node(
+            package='robot_proyecto',
+            executable='serial_bridge',
+            name='serial_bridge'
+        )
+    ])
