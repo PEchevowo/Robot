e8b92b750bc75c318b1fcbae51b762c47359fbc7
PEchevowo <paola.echev.s@gmail.com>
2026-08-01 17:25:04 -0400

    cayendo en la locura

--- proyecto_robot/src/robot_proyecto/launch/robot_real.launch.py
+++ proyecto_robot/src/robot_proyecto/launch/robot_real.launch.py
@@ -3,22 +3,35 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
-        # Nodo del LiDAR Hokuyo
+        # 1. Nodo del LiDAR Hokuyo
         Node(
             package='urg_node',
             executable='urg_node_driver',
             name='urg_node',
             parameters=[{
-                'serial_port': '/dev/ttyACM0', # Cambiar a ACM0 o ACM1 según corresponda
+                'serial_port': '/dev/ttyACM0',
                 'frame_id': 'laser_frame',
-                'angle_min': -2.35, # Los Hokuyo suelen tener mayor apertura
+                'angle_min': -2.35,
                 'angle_max': 2.35
             }]
         ),
-        # Nodo del puente serial para mover los motores vía Arduino
+        # 2. Nodo del puente serial (Músculos)
         Node(
             package='robot_proyecto',
             executable='serial_bridge',
             name='serial_bridge'
+        ),
+        # 3. Nodo de Odometría (Cálculo de movimiento)
+        Node(
+            package='robot_proyecto',
+            executable='odom_tf_publisher',
+            name='odom_tf_publisher'
+        ),
+        # 4. Transformada física (Une el láser al chasis del robot)
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='base_to_laser',
+            arguments=['0', '0', '0.1', '0', '0', '0', 'base_link', 'laser_frame']
         )
     ])
