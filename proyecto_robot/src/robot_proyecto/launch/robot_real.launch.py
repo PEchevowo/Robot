71b3803de2a930ef1af303af7c92dfe760dfd131
PEchevowo <paola.echev.s@gmail.com>
2026-08-02 11:01:38 -0400

    acotando vision lidar

--- proyecto_robot/src/robot_proyecto/launch/robot_real.launch.py
+++ proyecto_robot/src/robot_proyecto/launch/robot_real.launch.py
@@ -3,16 +3,16 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
-        # 1. Nodo del LiDAR Hokuyo (Con el nombre de frame corregido)
+        # 1. Nodo del LiDAR Hokuyo (Ajustado a 180 grados para ignorar el chasis)
         Node(
             package='urg_node',
             executable='urg_node_driver',
             name='urg_node',
             parameters=[{
                 'serial_port': '/dev/ttyACM0',
-                'frame_id': 'laser', 
-                'angle_min': -2.35,
-                'angle_max': 2.35
+                'frame_id': 'laser_frame',
+                'angle_min': -1.57,
+                'angle_max': 1.57
             }]
         ),
         # 2. Nodo del puente serial (Músculos)
@@ -21,18 +21,17 @@ def generate_launch_description():
             executable='serial_bridge',
             name='serial_bridge'
         ),
-        # 3. Transformada física (Une el láser al chasis del robot)
+        # 3. Nodo de Odometría (Cálculo de movimiento)
+        Node(
+            package='robot_proyecto',
+            executable='odom_tf_publisher',
+            name='odom_tf_publisher'
+        ),
+        # 4. Transformada física (Une el láser al chasis del robot)
         Node(
             package='tf2_ros',
             executable='static_transform_publisher',
             name='base_to_laser',
-            arguments=['0', '0', '0.1', '0', '0', '0', 'base_link', 'laser']
-        ),
-        # 4. Odometría Fija (Puente matemático para Nav2 y SLAM)
-        Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            name='odom_to_base',
-            arguments=['0', '0', '0', '0', '0', '0', 'odom', 'base_link']
+            arguments=['0', '0', '0.1', '0', '0', '0', 'base_link', 'laser_frame']
         )
     ])
